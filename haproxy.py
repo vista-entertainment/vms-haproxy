7b58caa28f2f7a9f1bbbada72877b44128634631
Vista Group <34108958+Vista-Group@users.noreply.github.com>
2019-04-11 17:30:15 +1200

    Update haproxy.py

--- haproxy.py
+++ haproxy.py
@@ -29,7 +29,7 @@ def get_tenant_by_name(tenant_name):
 	return new_tenant
 
 
-def get_backend_by_name(tenant, backend_name, hostname, pathprefix, bind='*:80', mode='http', port=80):
+def get_backend_by_name(tenant, backend_name, backend_dns, port=443):
 	for b in tenant['backends']:
 		if b['name'] == backend_name:
 			return b
@@ -37,10 +37,7 @@ def get_backend_by_name(tenant, backend_name, hostname, pathprefix, bind='*:80',
 	#print('create backend {0}'.format(backend_name))
 	new_backend = {}
 	new_backend['name']=backend_name
-	new_backend['hostname']=hostname
-	new_backend['bind']=bind
-	new_backend['pathprefix']=pathprefix
-	new_backend['mode']=mode
+	new_backend['dns']=backend_dns
 	new_backend['port']=port
 	new_backend['servers']=[]
 	tenant['backends'].append(new_backend)
@@ -50,23 +47,24 @@ def get_backend_by_name(tenant, backend_name, hostname, pathprefix, bind='*:80',
 def create_jsondata(azure_vm_json_data):
 	#look for owner of the VM and add it to the dict
 	for jd in azure_vm_json_data:
-		#grab the attributes that we want from the vm
-		azure_vm_tenant = jd['TENANT']
-		azure_vm_backend = jd['BACKEND']
-		azure_vm_ip = jd['Az_VNicPrivateIPs']
-		azure_vm_hostname = jd['HOSTNAME'] if 'HOSTNAME' in jd else None
-		azure_vm_pathprefix = jd['PATHPREFIX'] if 'PATHPREFIX' in jd else None
-		azure_vm_mode = jd['MODE'] if 'MODE' in jd else "http"
-		azure_vm_bind = jd['BIND'] if 'BIND' in jd else '*:80'
-		azure_vm_port = jd['PORT'] if 'PORT' in jd else '80'
+	
+		#ensure the tags are populated
+		if jd['TENANT'].strip() and jd['BACKEND'].strip() and jd['PORT'].strip():
+			
+			#grab the attributes that we want from the vm
+			azure_vm_tenant = jd['TENANT']
+			azure_vm_backend = jd['BACKEND']
+			azure_vm_dns = jd['DNS']
+			azure_vm_ip = jd['Az_VNicPrivateIPs']
+			azure_vm_port = jd['PORT'] if 'PORT' in jd else '443'
 
-		#fetch or create a tenant dict
-		current_tenant = get_tenant_by_name(azure_vm_tenant)
-		#add to new or existing tenant
-		current_backend = get_backend_by_name(current_tenant, azure_vm_backend, azure_vm_hostname, azure_vm_pathprefix, azure_vm_bind, azure_vm_mode, azure_vm_port)
+			#fetch or create a tenant dict
+			current_tenant = get_tenant_by_name(azure_vm_tenant)
+			#add to new or existing tenant
+			current_backend = get_backend_by_name(current_tenant, azure_vm_backend, azure_vm_dns, azure_vm_port)
 
-		#add ip and port to server list
-		current_backend['servers'].append("%s:%s" % (azure_vm_ip, azure_vm_port))
+			#add ip and port to server list
+			current_backend['servers'].append("%s:%s" % (azure_vm_ip, azure_vm_port))
 
 	return global_tenants
 
@@ -76,6 +74,9 @@ def render_template():
 	template = env.get_template('haproxy.j2')
 	
 	#print global_tenants
+	#print(len(global_tenants))
+	#print(global_tenants)
+
 	print(template.render(tenants=global_tenants))
 
 if __name__== "__main__":
